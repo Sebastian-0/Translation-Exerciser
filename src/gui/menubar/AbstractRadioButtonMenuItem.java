c393c1d361c3149868a524f41a942dfab058fcfb
Sebastian Hjelm <dat13shj@student.lu.se>
2018-05-16 18:47:31 +0200

    Fixed license year.

--- src/gui/menubar/AbstractRadioButtonMenuItem.java
+++ src/gui/menubar/AbstractRadioButtonMenuItem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Sebastian Hjelm
+ * Copyright (C) 2018 Sebastian Hjelm
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the MIT License.
